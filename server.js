e1fad76b71b13ad1668f0b5145790784155f4b44
oktavianussinyaru80-maker <oktavianussinyaru80@guru.sd.belajar.id>
2026-09-10 16:51:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
 // API: Ambil data dari Supabase
